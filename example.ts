d4db4889933c0e0522b39ba80f4117ad2412886b
Alex Oliveira <alexoliveira7x@gmail.com>
2023-04-19 19:12:34 +0100

    chore: sync settings

--- example.ts
+++ example.ts
@@ -1,30 +1,45 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
+import { acceptable, acceptWebSocket } from "https://deno.land/std/ws/mod.ts";
+
 const clients = new Map<string, WebSocket>();
 
-serve((req: Request) => {
-  const upgrade = req.headers.get("upgrade") || "";
-  if (upgrade.toLowerCase() != "websocket") {
-    return new Response("request isn't trying to upgrade to websocket.");
-  }
-  const { socket: ws, response } = Deno.upgradeWebSocket(req);
+const server = serve({ port: 8000 });
 
-  ws.onopen = () => console.log("socket opened");
-  ws.onmessage = (message) => {
-    const wsPayload: { type: string, data: string, mentions?: string[] } = JSON.parse(message.data);
-    
+console.log("Listening on http://localhost:8000");
+
+for await (const req of server) {
+  if (req.method === "GET" && req.url === "/ws" && acceptable(req)) {
+    acceptWebSocket({
+      conn: req.conn,
+      bufReader: req.r,
+      bufWriter: req.w,
+      headers: req.headers,
+    }).then(handleWebSocket);
+  } else {
+    req.respond({ status: 404, body: "Not found" });
+  }
+}
+
+function handleWebSocket(ws: WebSocket) {
+  console.log("Socket connected");
+
+  ws.onopen = () => console.log("Socket opened");
+
+  ws.onmessage = (message: MessageEvent) => {
+    const wsPayload: { type: string, data: string, mentions?: string[] } = JSON.parse(message.data.toString());
+
     if (wsPayload.type === "join") {
       clients.set(wsPayload.data, ws);
-      clients.forEach(client => client.send(JSON.stringify({ type: "message", data: `${wsPayload.data} Joined` })))
+      clients.forEach(client => client.send(JSON.stringify({ type: "message", data: `${wsPayload.data} Joined` })));
     }
 
-    if(wsPayload?.mentions && Array.isArray(wsPayload.mentions)) {
-      
-
+    if (wsPayload?.mentions && Array.isArray(wsPayload.mentions)) {
       wsPayload.mentions.forEach(mention => {
-        // if (clients.has(mention)) { // Testing only
-          clients.get(mention)?.send(JSON.stringify({ type: "message", data: wsPayload.data }));
-        // }
-      })
+        const client = clients.get(mention);
+        if (client) {
+          client.send(JSON.stringify({ type: "message", data: wsPayload.data }));
+        }
+      });
     }
   };
 
@@ -36,7 +51,4 @@ serve((req: Request) => {
       }
     });
   };
-  return response;
-});
-
-console.log("Listening on http://localhost:8000");
+}
