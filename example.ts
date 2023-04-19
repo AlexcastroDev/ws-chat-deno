ea078027f839a6410bcebc7138ad4b089ce0ef28
Alex Oliveira <alexoliveira7x@gmail.com>
2023-04-19 18:28:17 +0100

    chore: sync settings

--- example.ts
+++ example.ts
@@ -1,18 +1,38 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+const clients = new Map<string, WebSocket>();
 
-serve((req) => {
+serve((req: Request) => {
   const upgrade = req.headers.get("upgrade") || "";
   if (upgrade.toLowerCase() != "websocket") {
     return new Response("request isn't trying to upgrade to websocket.");
   }
-  const { socket, response } = Deno.upgradeWebSocket(req);
-  socket.onopen = () => console.log("socket opened");
-  socket.onmessage = (e) => {
-    console.log("socket message:", e.data);
-    socket.send(new Date().toString());
+  const { socket: ws, response } = Deno.upgradeWebSocket(req);
+  ws.onopen = () => console.log("socket opened");
+  ws.onmessage = (e) => {
+    const wsPayload: { type: string, data: string, mentions?: string[] } = JSON.parse(message);
+    
+    if (wsPayload.type === "join") {
+      clients.set(wsPayload.data, ws);
+      clients.forEach(client => client.send(JSON.stringify({ type: "message", data: `${wsPayload.data} Joined` })))
+    }
+
+    if(wsPayload?.mentions && Array.isArray(wsPayload.mentions)) {
+      wsPayload.mentions.forEach(mention => {
+        if (clients.has(mention)) {
+          clients.get(mention)?.send(JSON.stringify({ type: "message", data: wsPayload.data }));
+        }
+      })
+    }
   };
-  socket.onerror = (e) => console.log("socket errored:", e.message);
-  socket.onclose = () => console.log("socket closed");
+
+  ws.onclose = () => {
+    clients.forEach((client, key) => {
+      if (client === ws) {
+        clients.delete(key);
+        clients.forEach(c => c.send(JSON.stringify({ type: "message", data: `${key} Left` })));
+      }
+    });
+  };
   return response;
 });
 
