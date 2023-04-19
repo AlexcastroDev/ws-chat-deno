b96550120bfe5ea12d1c5d0fb8775f7532b8c88a
Alex Oliveira <alexoliveira7x@gmail.com>
2023-04-19 19:21:22 +0100

    chore: sync settings

--- example.ts
+++ example.ts
@@ -1,54 +1,32 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-import { acceptable, acceptWebSocket } from "https://deno.land/std/ws/mod.ts";
+import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
 
-const clients = new Map<string, WebSocket>();
+const clients = new Map<string, WebSocketClient>();
+const wss = new WebSocketServer();
 
-const server = serve({ port: 8000 });
-
-console.log("Listening on http://localhost:8000");
-
-for await (const req of server) {
-  if (req.method === "GET" && req.url === "/ws" && acceptable(req)) {
-    acceptWebSocket({
-      conn: req.conn,
-      bufReader: req.r,
-      bufWriter: req.w,
-      headers: req.headers,
-    }).then(handleWebSocket);
-  } else {
-    req.respond({ status: 404, body: "Not found" });
-  }
-}
-
-function handleWebSocket(ws: WebSocket) {
-  console.log("Socket connected");
-
-  ws.onopen = () => console.log("Socket opened");
-
-  ws.onmessage = (message: MessageEvent) => {
-    const wsPayload: { type: string, data: string, mentions?: string[] } = JSON.parse(message.data.toString());
-
+wss.on("connection", function (ws: WebSocketClient) {
+  ws.on("message", function (message: string) {
+    const wsPayload: { type: string, data: string, mentions?: string[] } = JSON.parse(message);
+    
     if (wsPayload.type === "join") {
       clients.set(wsPayload.data, ws);
-      clients.forEach(client => client.send(JSON.stringify({ type: "message", data: `${wsPayload.data} Joined` })));
+      clients.forEach(client => client.send(JSON.stringify({ type: "message", data: `${wsPayload.data} Joined` })))
     }
 
-    if (wsPayload?.mentions && Array.isArray(wsPayload.mentions)) {
+    if(wsPayload?.mentions && Array.isArray(wsPayload.mentions)) {
       wsPayload.mentions.forEach(mention => {
-        const client = clients.get(mention);
-        if (client) {
-          client.send(JSON.stringify({ type: "message", data: wsPayload.data }));
+        if (clients.has(mention)) {
+          clients.get(mention)?.send(JSON.stringify({ type: "message", data: wsPayload.data }));
         }
-      });
+      })
     }
-  };
+  });
 
-  ws.onclose = () => {
+  ws.on("close", function () {
     clients.forEach((client, key) => {
       if (client === ws) {
         clients.delete(key);
         clients.forEach(c => c.send(JSON.stringify({ type: "message", data: `${key} Left` })));
       }
     });
-  };
-}
+  });
+});
